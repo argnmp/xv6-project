aeadc79a8c270cdabd27868c1477291ba9d1893d
2019097210 <kimtahen@hanyang.ac.kr>
2023-05-27 14:19:54 +0900

    reset test_app and sub_app

--- xv6-project2/test_app.c
+++ xv6-project2/test_app.c
@@ -2,78 +2,8 @@
 #include "stat.h"
 #include "user.h"
 #define dbg(fmt, args...) printf(1, "[%d: %s] pid %d | " fmt "\n",__LINE__, __FUNCTION__, getpid(), ##args)
-#define WORKER 9
-#define WORKER2 4
-#define PAGE_SIZE 4096
-int listcmd(){
-  struct proc_info_s pinfos; 
-  int res = procinfo(&pinfos);
-  if(res < 0) return -1;  
-  for(int i = 0; i < pinfos.pcount; i++){
-    printf(1, "pid: %d | pname: %s | number of stack pages: %d | allocated memory size: %d | memory limit %d\n", pinfos.proc_arr[i].pid, pinfos.proc_arr[i].pname, pinfos.proc_arr[i].ssz / PAGE_SIZE, pinfos.proc_arr[i].sz, pinfos.proc_arr[i].sz_limit); 
-
-  }
-  return 0;
-}
-
-/* void* temp2(void* args){
-  int* ret = (int*)((int)200+(int)args);
-  // dbg("temp2");
-  //dbg("before thread_exit()");
-  thread_exit(ret);
-  return 0;
-}
-void* temp1(void* args){
-  int* ret = (int*)((int)200+(int)args);
-  // int dum = 1;
-  int pid = fork();
-  // dbg("temp1");
-
-  if(pid == 0){
-    // dbg("in th fork");
-
-    int res, retval = 0;
-    thread_t tids[WORKER2] = {0,};
-    //dbg("address of dum: %d, address of tids: %d\n", &dum, tids);
-    for(int i = 0; i<WORKER2; i++){
-      res = thread_create(&tids[i], temp2, (int*)i);
-      if(res==-1)
-        dbg("th create -> res: %d",res);
-    }
-    for(int i = 0; i<WORKER2; i++){
-      res = thread_join(tids[i], (void*)&retval);
-      if(res==-1)
-        dbg("th join -> tid: %d, res: %d, retval: %d", tids[i], res, retval);
-    }
-    //dbg("before exit()");
-    exit();
-  }
-  // dbg("after fork wait");
-  wait();
-  //dbg("before thread_exit()");
-  thread_exit(ret);
-  return 0;
-}
-thread_t tids[WORKER] = {0,};
-int main(int argc, char * argv[]){
-  int retval = 0;
-  // int res;
-  for(int i = 0; i<WORKER; i++){
-    thread_create(&tids[i], temp1, (int*)i);
-    // dbg("th create, res: %d", res);
-  }
-  for(int i = 0; i<WORKER; i++){
-    //dbg("1 / tid: %d, th join", tids[i]);
-    thread_join(tids[i], (void*)&retval);
-    //dbg("2 / tid: %d, th join", tids[i]);
-    //dbg("tid:%d, th join, res: %d, retval: %d",tids[i], res, retval);
-  }
-  dbg("test_app success\n");
-  exit();
-} */
 
 int main(int argc, char * argv[]){
   dbg("----helloworld----");
-  sleep(10000);
   exit();
 }
