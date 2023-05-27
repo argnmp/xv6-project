38fcfeb76ae9bf64ba46f7f55420c6063dcbae87
2019097210 <kimtahen@hanyang.ac.kr>
2023-05-27 14:18:47 +0900

    comments are added in pmanager

--- xv6-project2/pmanager.c
+++ xv6-project2/pmanager.c
@@ -17,6 +17,9 @@ int get_input(char* buf, int buf_size){
   return 0;
 }
 int listcmd(){
+  /*
+   * print the current state of processes
+   */
   struct proc_info_s pinfos; 
   int res = procinfo(&pinfos);
   if(res < 0) return -1;  
@@ -34,10 +37,12 @@ int run(struct args_s* args){
     }
   }
   else if(strcmp(args->arg[0], "kill")==0){
+    // check arguments are empty
     if(strcmp(args->arg[1], "")==0){
       return 0; 
     }
 
+    // kill target pid 
     int pid = atoi(args->arg[1]); 
     int res = kill(pid);
     if(res==0){
@@ -48,10 +53,14 @@ int run(struct args_s* args){
     }
   }
   else if(strcmp(args->arg[0], "execute")==0){
-    // check path is empty
+    // check arguments are empty
     if(strcmp(args->arg[1], "")==0 || strcmp(args->arg[2], "")==0){
       return 0; 
     }
+
+    /*
+     * fork twice so that the pmanager continues to work
+     */
     int pid;
     pid = fork();
     char* execargv[10]; 
@@ -61,8 +70,12 @@ int run(struct args_s* args){
 
     int stacksize = atoi(args->arg[2]);
     if(pid==0){
+      /*
+       * second fork to execute program
+       */
       int pid2 = fork();
       if(pid2==0){
+        // execute program with stacksize
         int res = exec2(execargv[0], execargv, stacksize); 
         if(res < 0){
           printf(1, "execute failed\n");
@@ -71,8 +84,11 @@ int run(struct args_s* args){
       }
       else if(pid2 < 0){
         printf(1, "execute failed\n");
+        exit();
       }
       else{
+        // exit immediately
+        // do not wait for the executed program
         exit();
       }
     }
@@ -80,16 +96,22 @@ int run(struct args_s* args){
       printf(1, "execute failed\n");
     }
     else {
+      // wait for the second fork to finish
+      /*
+       * immediately return to pmanager, 
+       * does not wait for the executed program to finish
+       */
       wait();
     }
   }
   else if(strcmp(args->arg[0], "memlim")==0){
-    // check path is empty
+    // check arguments are empty
     if(strcmp(args->arg[1], "")==0 || strcmp(args->arg[2], "")==0){
       return 0; 
     }
     int t_pid = atoi(args->arg[1]);
     int t_limit = atoi(args->arg[2]);
+    // call setmemorylimit for target process
     int res = setmemorylimit(t_pid, t_limit);
     if(res==0){
       printf(1, "memlim succeeded\n");
@@ -99,6 +121,7 @@ int run(struct args_s* args){
     }
   }
   else if(strcmp(args->arg[0], "exit")==0){
+    // exit pmanager
     exit(); 
   } 
   return 0;
@@ -110,7 +133,6 @@ char stv[] = " \t\v";
 int main(){
   static char buf[100];     
   while(get_input(buf, sizeof(buf)) == 0){
-    // parse command
     char* pstr = buf;
     char* str = buf;
     struct args_s args;
@@ -118,6 +140,9 @@ int main(){
     args.arg[1] = "\0";
     args.arg[2] = "\0";
 
+    /*
+     * parse commands
+     */
     for(int count = 0; count <=2; count+=1){
       for(;strchr(stv, *str)!=0; str+=1);
       pstr = str;
@@ -134,6 +159,9 @@ int main(){
       pstr = str;
     }
     
+    /*
+     * run command using the parsed command
+     */
     run(&args);
   }
   exit(); 
