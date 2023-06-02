85a1771f62fc941c68fcb86b40e3a53d08454d81
2019097210 <kimtahen@hanyang.ac.kr>
2023-06-02 12:59:30 +0900

    debug setting

--- xv6-project3/types.h
+++ xv6-project3/types.h
@@ -2,3 +2,6 @@ typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 typedef uint pde_t;
+
+#define cdbg(fmt, args...) cprintf("[%d: %s] | " fmt "\n",__LINE__, __FUNCTION__, ##args)
+#define udbg(fmt, args...) printf(1, "[%d: %s] pid %d | " fmt "\n",__LINE__, __FUNCTION__, getpid(), ##args)
